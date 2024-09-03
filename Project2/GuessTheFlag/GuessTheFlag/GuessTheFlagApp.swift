37244c047add8e267c4c36467ad5ae90726386c3
matthewzierl <matthewtzierl@gmail.com>
2024-09-03 12:20:46 -0500

    Draw with Core Graphics
    Create 7 different drawings that make use of Core
    Graphics including simple lines with .moveto and
    .addLine. Rotating squares and filling in with
    stroke and fill. All images were made with a
    closure using UIGraphicsImageRenderer.image.

---
+++ Project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Matthew Zierl on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
