7e66dc019c5b4bd1b008ef46ddff2815a9548ff2
matthewzierl <matthewtzierl@gmail.com>
2024-09-06 09:50:43 -0500

    RPS NPC prediction and win/lose logic
    Since the UI is finished, I implemented like 4
    alerts to notify the user they haven't made a
    prediction, result of match, result of game, and
    prompt for the user to choose if they plan to win
    or lose. Anyways, the game is over. What I'm
    getting from this is SwiftUI is just a lot "looser"
    I guess than UIKit. I still have some getting used
    to.

--- Project1-3Milestone/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ Project1-3Milestone/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
+    enum Prediciton {
+        case none, win, lose
+    }
+    
     struct ImageView: View {
         
         var imageName: String
@@ -23,9 +27,20 @@ struct ContentView: View {
         }
     }
     
+    let images = ["rock", "paper", "scissors"]
+    @State private var currentChoice: String = ""
+    @State private var npcChoice: String = ""
+    @State private var result: String = ""
+    @State private var showingChoice = false
+    @State private var showingPrompt = false
+    @State private var showingResults = false
+    @State private var showFinalScore = false
+    @State private var prediction: Prediciton = .none
+    @State private var turn = 1
+    @State private var score = 0
+    
     var body: some View {
         
-        let images = ["rock", "paper", "scissors"]
         
         NavigationStack {
             ZStack {
@@ -35,20 +50,38 @@ struct ContentView: View {
                 
                 VStack {
                     HStack {
-                        Text("Score: 0")
                         Spacer()
-                        Text("Rock Paper Scissors")
+                        Text("Score: \(score)")
                         Spacer()
-                        Text("Turn: 0")
+                        Text("Turn: \(turn)")
+                        Spacer()
                     }
+                    
+                    Spacer()
+                    Text("Your Prediction: \(getPredicition())")
+                    Button {
+                        showingChoice = true
+                    } label: {
+                        Text("Make Prediction")
+                    }
+                    .alert("Win or Lose?", isPresented: $showingChoice) {
+                        Button("Win") {
+                            prediction = .win
+                        }
+                        Button("Lose") {
+                            prediction = .lose
+                        }
+                    } message: {
+                        Text("Make your prediction before choosing.")
+                    }
+
                     
                     Spacer()
                     
                     VStack {
                         ForEach(images, id: \.self) { image in
-                            
                             Button(action: {
-                                submitChoice()
+                                submitChoice(choice: image)
                             }, label: {
                                 ImageView(imageName: image)
                             })
@@ -60,21 +93,124 @@ struct ContentView: View {
                 }
                 
             }
-            .navigationTitle("TLSKJF:LJDLF")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: {
-                        print("Button pressed!")
+                        startNewGame()
                     }) {
-                        Image(systemName: "plus")
+                        Image(systemName: "repeat")
+                            .foregroundStyle(.black)
                     }
                 }
             }
         }
+        .alert("Please Make a Prediction First", isPresented: $showingPrompt) {
+            Button("Okay") { }
+        }
+        .alert("Results", isPresented: $showingResults) {
+            Button("Okay") { startNextTurn() }
+        } message: {
+            Text("Your Prediction: \(getPredicition())\nYour Move: \(currentChoice) | NPC Move: \(npcChoice)\nYou were \(result)!")
+        }
+        .alert("Game Over", isPresented: $showFinalScore) {
+            Button("New Game") {
+                startNewGame()
+            }
+        } message: {
+            Text("Final Score: \(score)")
+        }
     }
     
-    func submitChoice() {
+    func getPredicition() -> String {
+        switch prediction {
+        case .none:
+            return "none"
+        case .win:
+            return "win"
+        case .lose:
+            return "lose"
+        }
+    }
+    
+    func submitChoice(choice: String) {
         
+        guard prediction != .none else {
+            showingPrompt = true
+            return
+        }
+        
+        currentChoice = choice
+        
+        npcChoice = images[Int.random(in: 0..<3)]
+        
+        switch currentChoice {
+        case "rock":
+            if prediction == .win {
+                if npcChoice == "scissors" {
+                    score += 1
+                    result = "Correct"
+                } else {
+                    result = "Wrong"
+                }
+            } else { // lose
+                if npcChoice == "paper" {
+                    score += 1
+                    result = "Correct"
+                } else {
+                    result = "Wrong"
+                }
+            }
+        case "paper":
+            if prediction == .win {
+                if npcChoice == "rock" {
+                    score += 1
+                    result = "Correct"
+                } else {
+                    result = "Wrong"
+                }
+            } else { // lose
+                if npcChoice == "scissors" {
+                    score += 1
+                    result = "Correct"
+                } else {
+                    result = "Wrong"
+                }
+            }
+        case "scissors":
+            if prediction == .win {
+                if npcChoice == "paper" {
+                    score += 1
+                    result = "Correct"
+                } else {
+                    result = "Wrong"
+                }
+            } else { // lose
+                if npcChoice == "rock" {
+                    score += 1
+                    result = "Correct"
+                } else {
+                    result = "Wrong"
+                }
+            }
+            
+        default:
+            break
+        }
+        
+        showingResults = true
+    }
+    
+    func startNextTurn() {
+        turn += 1
+        prediction = .none
+        if turn > 10 {
+            showFinalScore = true
+        }
+    }
+    
+    func startNewGame() {
+        turn = 1
+        score = 0
     }
 }
 
