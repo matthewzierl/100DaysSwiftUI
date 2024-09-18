3dd45907bd7bb4b6ad6e8b7b7132fe78a81086cd
matthewzierl <matthewtzierl@gmail.com>
2024-09-17 23:39:36 -0500

    iExpense Source Files
    Create boilerplate for iExpense app.

---
+++ Project7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matthew Zierl on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
