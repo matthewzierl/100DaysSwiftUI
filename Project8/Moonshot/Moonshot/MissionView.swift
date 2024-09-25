d8f0e4202ae87bb5f0ac49813e8dbb503855668a
matthewzierl <matthewtzierl@gmail.com>
2024-09-24 23:30:50 -0500

    Moonshot AstronautView
    Create new view for when user selects a member
    under mission details. This view contains an image
    of the crew member as well as their description.

--- Project8/Moonshot/Moonshot/MissionView.swift
+++ Project8/Moonshot/Moonshot/MissionView.swift
@@ -55,7 +55,7 @@ struct MissionView: View {
                 HStack {
                     ForEach(crew, id: \.role) { crewMember in
                         NavigationLink {
-                            Text("Astronaut Details")
+                            AstronautView(astronaut: crewMember.astronaut)
                         } label: {
                             Image(crewMember.astronaut.id)
                                 .resizable()
