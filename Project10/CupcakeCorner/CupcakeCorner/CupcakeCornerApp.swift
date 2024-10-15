e48b5d82427213ffbfeabc7b954d2f19edbf3268
matthewzierl <matthewtzierl@gmail.com>
2024-10-14 19:29:13 -0500

    Use Async Await to Pull Data and Images, Disabling
    Use async methods to signify that the method will
    take time and can be put to sleep. Use await when
    calling those methods to signify sleep may occur.
    We use these so that loading things from the
    internet doesn't interupt the UI of our app.
    Also learned .disable modifier on forms to disable
    buttons or other things that require user input so
    that we can only enable when a certain condition
    has been met.

---
+++ Project10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Matthew Zierl on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
