5631111ccb1d811241da4937f11e97380452101a
matthewzierl <matthewtzierl@gmail.com>
2025-01-06 01:12:52 -0600

    Swift Data Filtering (Predicate)
    When using @Query to pull a model from SwiftData,
    you can use a filter: #Predicate<Model> {} to
    filter through models. Additionally, I was having
    a hard time figuring out why the model context
    wasn't automatically saving models (as it should
    when initializing from App using .modelContainer)
    and from what I found out, saving occurs when
    SwiftData deems it appropriate based on the app
    lifecycle which may not always align with what
    you need for the app. As a result, explicitly
    saving at crucial parts in your app may be
    necessary.

---
+++ Project12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Matthew Zierl on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project12/SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Matthew Zierl on 1/5/25.
+//
+
+import Foundation
